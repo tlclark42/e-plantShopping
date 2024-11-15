e56bd1194e2f23f2b33680c3285c6039cc6f7247
tlclark42 <tlclark42@gmail.com>
2024-11-14 23:42:40 -0500

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
+import ProductList from './ProductList';
+import CartItem from './CartItem';
 
 export const CartSlice = createSlice({
   name: 'cart',
@@ -7,17 +9,45 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        alert('inside');
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+          existingItem.quantity++;
+        } else {
+          state.items.push({ name, image, cost, quantity: 1 });
+        }
+       
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+        const { name, quantity } = action.payload;
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate) {
+          itemToUpdate.quantity = quantity;
+        }
     },
   },
 });
 
-export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+/* export  default function appReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'todos/todoAdded': {
+          return {
+    addItem, 
+    removeItem, 
+    updateQuantity, 
+} = CartSlice.actions;
 
-export default CartSlice.reducer;
+ export const { 
+    removeitem, 
+    updatequantity, 
+} = ( removeItem, updateQuantity) => CartItem.actions;
+
+*/
+export const { additem } =  ProductList.actions;
+
+export default CartSlice.reducer; 
+//export const Store = () =>{};
